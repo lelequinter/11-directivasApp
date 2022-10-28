8a4b6c159c88e34df964ac5eaece851d2a1dd90c
Leyder Quintero <lele.quinter@gmail.com>
2022-10-28 10:22:39 -0500

    Input setters

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,7 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarComponent implements OnInit {
 
-  texto1: string = 'leyder'
+  texto1: string = 'leyder';
+  color: string = 'green';
 
   miFormulario: FormGroup = this.fb.group(
     {
@@ -26,8 +27,13 @@ export class AgregarComponent implements OnInit {
     return this.miFormulario.get(campo)?.invalid || false;
   }
 
-  cambiarNombre(){
-    this.texto1 = 'Quintero'
+  cambiarNombre() {
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor() {
+    const color = "#xxxxxx".replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    this.color = color
   }
 
 }
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -5,8 +5,17 @@ import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '
 })
 export class ErrorMsgDirective implements OnInit, OnChanges {
 
-  @Input() color: string = 'red';
-  @Input() mensaje: string = 'Campo requerido';
+  private _color: string = 'red';
+  @Input() set color(valor: string) {
+    this.htmlElement.nativeElement.style.color = valor;
+    this._color = valor;
+  }
+  // @Input() mensaje1: string = 'Campo requerido';
+  private _mensaje: string = 'Campo requerido'
+  @Input() set mensaje(msg: string) {
+    this.htmlElement.nativeElement.innerText = msg;
+    this._mensaje = msg;
+  };
 
   htmlElement: ElementRef<HTMLElement>;
 
@@ -15,22 +24,30 @@ export class ErrorMsgDirective implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const mensaje = changes["mensaje"].currentValue;
-    console.log(mensaje);
-    this.setMsg();
+    // const mensaje = changes["mensaje"].currentValue;
+    // console.log(mensaje);
+    // this.setMsg();
+    // if( changes["color"] ){
+    //   const color = changes["color"].currentValue;
+    //   this.htmlElement.nativeElement.style.color = color;
+    // }
   }
 
   ngOnInit(): void {
-    this.setColor();
-    this.setMsg();
+    // this.setColor();
+    // this.setMsg();
+    this.setEstilo();
   }
 
-  setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color;
+  setEstilo(): void {
     this.htmlElement.nativeElement.classList.add('form-text');
   }
 
-  setMsg(): void {
-    this.htmlElement.nativeElement.innerText = this.mensaje;
-  }
+  // setColor(): void {
+  //   this.htmlElement.nativeElement.style.color = this.color;
+  // }
+
+  // setMsg(): void {
+  //   this.htmlElement.nativeElement.innerText = this.mensaje;
+  // }
 }
